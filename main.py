1cf30c98f14bba81c7bd4da5a3da3c7d184f4ebc
Cupid_www <dearhoshino@gmail.com>
2023-12-05 00:07:57 +0800

    算路，显示结果完成

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 import GUI
 import graph
+import graph as G
 import source
+
+stop_list = []  # list of stops: [[line, station], ...]
+result_list = []  # [str, str, ...]
+result_route = ''
+total_time = 0
 
 
 def add_item_cb(cb: QtWidgets.QComboBox, data_list: list):
     for item in data_list:
         cb.addItem(item)
+
 
 def add_by_line(cb: QtWidgets.QComboBox, line: str):
     cb.clear()
@@ -19,19 +26,17 @@ def add_by_line(cb: QtWidgets.QComboBox, line: str):
     elif line == '3':
         add_item_cb(cb, source.l3)
 
+
 def show_start_list():
     line = UI.start_line.currentText()
     dest = UI.start_station
     add_by_line(dest, line)
 
+
 def show_end_list():
     line = UI.end_line.currentText()
     dest = UI.end_station
     add_by_line(dest, line)
-
-
-
-
 
 
 def add_column(col: int, header: str, table):
@@ -61,6 +66,7 @@ def add_stopover():
 
     def add_tmp():
         add_by_line(cb_station, cb_line.currentText())
+
     cb_line.currentIndexChanged.connect(add_tmp)
 
     table = UI.stopover_list
@@ -79,6 +85,97 @@ def del_stopover():
         table.setColumnCount(0)
 
 
+def remove_blank():
+    table = UI.stopover_list
+    for i in range(table.rowCount() - 1, -1, -1):
+        if table.cellWidget(i, 0).currentIndex() == 0:
+            table.removeRow(i)
+
+
+def calculate(start: str, end: str):
+    global result_route
+    prf = UI.preference.currentIndex()
+    if prf == 0:
+        d_result = G.dijkstra(G.graph, start, end)
+    else:
+        d_result = G.dijkstra(G.graph2, start, end)
+    return d_result
+
+
+def is_transfer(station: str):
+    return station in source.l1Tl2 + source.l1Tl3 + source.l2Tl3
+
+
+def get_stop_list():
+    global stop_list
+    stop_list.clear()
+    stop = [UI.start_line.currentText(), UI.start_station.currentText()]
+    stop_list.append(stop.copy())
+    table = UI.stopover_list
+    if table.rowCount() > 0:
+        for r in range(table.rowCount()):
+            stop.clear()
+            stop.append(table.cellWidget(r, 0).currentText())
+            stop.append(table.cellWidget(r, 1).currentText())
+            stop_list.append(stop.copy())
+    stop.clear()
+    stop = [UI.end_line.currentText(), UI.end_station.currentText()]
+    stop_list.append(stop)
+    print(stop_list)
+
+
+def calculate_all():
+    global stop_list
+    global result_list
+    global total_time
+    result_list.clear()
+    total_time = 0
+    prf = UI.preference.currentIndex()
+    print(stop_list)
+    for i in range(len(stop_list) - 1):
+        start = stop_list[i][1]
+        end = stop_list[i + 1][1]
+        print(start)
+        print(end)
+        if prf == 1:
+            if is_transfer(start):
+                start += stop_list[i][0]
+            if is_transfer(end):
+                end += stop_list[i][0]
+            result = graph.dijkstra(G.graph2, start, end)
+            print(result)
+        else:
+            result = graph.dijkstra(G.graph, start, end)
+            print(result)
+        if type(result) == str:
+            result_list.clear()
+            return result
+        else:
+            result_list.append(result[0])
+            total_time += result[1]
+    print(result_list)
+    return result_list
+
+
+def show_result():
+    global total_time
+    show_str = ''
+    UI.result.setText('')
+    for i in range(len(result_list)):
+        show_str += f'{stop_list[i][1]} to {stop_list[i + 1][1]}:\n'
+        show_str += f'{result_list[i]}\n\n'
+    show_str += f'Total time: {total_time}'
+    UI.result.setText(show_str)
+
+
+def calculate_route_slot():
+    remove_blank()
+    get_stop_list()
+    calculate_all()
+    show_result()
+    UI.stack.setCurrentIndex(1)
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     Main = QtWidgets.QMainWindow()
@@ -87,14 +184,16 @@ if __name__ == '__main__':
 
     add_item_cb(UI.start_line, ['', '1', '2', '3'])
     add_item_cb(UI.end_line, ['', '1', '2', '3'])
-
-
+    add_item_cb(UI.preference, ['shortest time', 'minimize transfers'])
+    UI.preference.setCurrentIndex(0)
 
     UI.start_line.currentIndexChanged.connect(show_start_list)
     UI.end_line.currentIndexChanged.connect(show_end_list)
     UI.add_button.clicked.connect(add_stopover)
     UI.delete_button.clicked.connect(del_stopover)
-
+    UI.calculate.clicked.connect(calculate_route_slot)
+    UI.return_home.clicked.connect(lambda turn: UI.stack.setCurrentIndex(0))
+    UI.return_home_2.clicked.connect(lambda turn: UI.stack.setCurrentIndex(0))
 
     UI.stack.setCurrentIndex(0)
     Main.show()
